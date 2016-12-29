2d67ade311bd69aa4e43c538231de0ab17606148
djekanovic <djekanovic@rtrkbl18.domain.local>
2016-12-29 10:13:09 +0100

    Client [HTTP CLIENT, SOCKET]

--- src/main/java/com/rtrk/client/socket/ServerSocket.java
+++ src/main/java/com/rtrk/client/socket/ServerSocket.java
@@ -1,20 +1,16 @@
 package com.rtrk.client.socket;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.net.Socket;
 
 public class ServerSocket extends Thread {
 
 	private static final int TCP_PORT = 9090;
-	private static final String filePath = ".\\reqsocket";
-	private static final int bufferSize = 65;
+	private static final String filePath = "\\reqsocket";
+	private static final int bufferSize = 1024;
 
 	public void run() {
 		try {
@@ -23,17 +19,16 @@ public class ServerSocket extends Thread {
 			while (!com.rtrk.client.App.end) {
 				Socket socket = serverSocket.accept();
 				System.out.println("Client accepted");
-				BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-				PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())),
-						true);
+				DataInputStream in=new DataInputStream(socket.getInputStream());
+				DataOutputStream out=new DataOutputStream(socket.getOutputStream());
 				// Receive file length
-				int fileLength = Integer.parseInt(in.readLine());
+				int fileLength = in.readInt();
+				System.out.println("File length: "+fileLength);
 				// Receive bytes
-				InputStream inBytes = socket.getInputStream();
-				FileOutputStream fout = new FileOutputStream(new File(filePath));
+				FileOutputStream fout = new FileOutputStream(new File(filePath), true);
 				byte[] bytes = new byte[bufferSize];
 				int received = 0, count;
-				while ((count = inBytes.read(bytes)) > 0) {
+				while ((count = in.read(bytes)) > 0) {
 					fout.write(bytes, 0, count);
 					received += count;
 					if (fileLength == received) {
@@ -41,11 +36,9 @@ public class ServerSocket extends Thread {
 					}
 				}
 				// Send response status
-				out.println("OK");
+				out.writeBoolean(true);
 				fout.close();
-				inBytes.close();
 				in.close();
-				out.close();
 				socket.close();
 			}
 			serverSocket.close();
